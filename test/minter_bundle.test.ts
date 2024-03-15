0f660373499ebffb7e7d12bf8332aa4e73e792f0
Marco Jardim <marcoeojardim@gmail.com>
2024-03-15 00:57:10 -0300

    Add TreasuryModule import, deploy Treasury, set treasury in MinterBundle, grant permission to treasury, update tests for minting with ETH value and revert conditions. Set KNFT price, change treasury address, kBox address, KNFT address, bundle size, set DNA of KNFT after minting, set gene at specific position.

--- test/minter_bundle.test.ts
+++ test/minter_bundle.test.ts
@@ -4,6 +4,7 @@ const { ethers, ignition } = require("hardhat");
 import KNFTModule from "../ignition/modules/KNFTModule";
 import Minter_BundleModule from "../ignition/modules/Minter_BundleModule";
 import KBoxModule from '../ignition/modules/KBoxModule';
+import Treasury from '../ignition/modules/TreasuryModule';
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 
 describe("MinterBundle", () => {
@@ -11,6 +12,7 @@ describe("MinterBundle", () => {
     const minter = await ignition.deploy(Minter_BundleModule);
     const knft = await ignition.deploy(KNFTModule);
     const kbox = await ignition.deploy(KBoxModule);
+    const treasury = await ignition.deploy(Treasury);
 
     const MINTER_ROLE = ethers.keccak256(ethers.toUtf8Bytes("MINTER_ROLE"));
     
@@ -19,6 +21,12 @@ describe("MinterBundle", () => {
 
     const setKBox = await minter.minterBundle.setKBox(kbox.kBox);
     await setKBox.wait();
+
+    const setTreasury = await minter.minterBundle.setTreasury(treasury.treasury);
+    await setTreasury.wait();
+
+    const treasuryPermission = await treasury.treasury.setPermission(0, minter.minterBundle, true);
+    await treasuryPermission.wait();
     
     const grantRole = await knft.kondux.grantRole(MINTER_ROLE, minter.minterBundle);
     await grantRole.wait();
@@ -26,7 +34,7 @@ describe("MinterBundle", () => {
     const [owner] = await ethers.getSigners();
     const ownerAddress = await owner.getAddress();
 
-    return { minter, knft, kbox, owner, ownerAddress };
+    return { minter, knft, kbox, owner, ownerAddress, treasury };
   }
   
   beforeEach(async () => {
@@ -41,12 +49,14 @@ describe("MinterBundle", () => {
     const { minter, knft, ownerAddress } = await loadFixture(deployModuleFixture);
 
     const mint = await minter.minterBundle.publicMint({value: ethers.parseEther("0.25")});
-    await mint.wait();
+    const mintReceipt = await mint.wait();
 
     expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(5);
     for (let index = 0; index < 5; index++) {
       expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
     }
+
+    await expect(minter.minterBundle.publicMint({value: ethers.parseEther("0")})).to.be.revertedWith("Not enough ETH sent");
 
     await expect(minter.minterBundle.publicMint({value: ethers.parseEther("0.00001")})).to.be.revertedWith("Not enough ETH sent");
 
@@ -76,17 +86,285 @@ describe("MinterBundle", () => {
     await approve.wait();
 
     const mint = await minter.minterBundle.publicMintWithBox(1);
+    const mintReceipt = await mint.wait();
+
+    expect(await kbox.kBox.balanceOf(ownerAddress)).to.equal(0);
+    expect(await kbox.kBox.totalSupply()).to.equal(0);
+
+    expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(5);
+    for (let index = 0; index < 5; index++) {
+      expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
+    }
+
+    await expect(minter.minterBundle.publicMintWithBox(1)).to.be.reverted;
+      
+  });
+
+  it("Should set the price of KNFT", async () => {
+    const { minter, knft, ownerAddress } = await loadFixture(deployModuleFixture);
+    
+    expect(await minter.minterBundle.price()).to.equal(ethers.parseEther("0.25"));
+
+    const mint = await minter.minterBundle.publicMint({value: ethers.parseEther("0.25")});
+    await mint.wait();
+
+    expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(5);
+    for (let index = 0; index < 5; index++) {
+      expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
+    }
+
+    const setPrice = await minter.minterBundle.setPrice(ethers.parseEther("0.5"));
+    const setPriceReceipt = await setPrice.wait();    
+
+    expect(await minter.minterBundle.price()).to.equal(ethers.parseEther("0.5"));
+
+    await expect(minter.minterBundle.publicMint({value: ethers.parseEther("0.25")})).to.be.revertedWith("Not enough ETH sent");
+
+    const mint2 = await minter.minterBundle.publicMint({value: ethers.parseEther("0.5")});
+    await mint2.wait();
+
+    expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(10);
+    for (let index = 5; index < 10; index++) {
+      expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
+    }
+  });
+
+  it("Should change the treasury address", async () => {
+    const { minter, knft, treasury, ownerAddress } = await loadFixture(deployModuleFixture);
+
+    const treasuryMinterAddress = await minter.minterBundle.getTreasury();
+    const treasuryContractAddress = await treasury.treasury.target;
+
+    expect(treasuryMinterAddress).to.equal(treasuryContractAddress);
+
+    const mint = await minter.minterBundle.publicMint({value: ethers.parseEther("0.25")});
+    await mint.wait();
+
+    expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(5);
+    for (let index = 0; index < 5; index++) {
+      expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
+    }
+
+    const treasury2 = await ignition.deploy(Treasury);
+    const setTreasury = await minter.minterBundle.setTreasury(treasury2.treasury);
+    const setTreasuryReceipt = await setTreasury.wait();
+
+    const treasuryPermission = await treasury2.treasury.setPermission(0, minter.minterBundle, true);
+    await treasuryPermission.wait();
+
+    expect(await minter.minterBundle.getTreasury()).to.equal(treasury2.treasury);
+
+    const mint2 = await minter.minterBundle.publicMint({value: ethers.parseEther("0.25")});
+    await mint2.wait();
+
+    expect(await ethers.provider.getBalance(treasury2.treasury)).to.equal(ethers.parseEther("0.25"));
+  });
+
+  it("Should change the kBox address", async () => {
+    const { minter, knft, kbox, ownerAddress } = await loadFixture(deployModuleFixture);
+
+    const kBoxAddress = await minter.minterBundle.getKBox();
+    const kBoxContractAddress = await kbox.kBox.target;
+
+    expect(kBoxAddress).to.equal(kBoxContractAddress);
+
+    const mintBox = await kbox.kBox.faucet();
+    await mintBox.wait();
+
+    expect(await kbox.kBox.balanceOf(ownerAddress)).to.equal(1);
+    expect(await kbox.kBox.totalSupply()).to.equal(1);
+    expect(await kbox.kBox.ownerOf(1)).to.equal(ownerAddress);
+
+    await expect(minter.minterBundle.publicMintWithBox(0)).to.be.reverted;
+
+    const approve = await kbox.kBox.approve(minter.minterBundle, 1);
+    await approve.wait();
+
+    const mint = await minter.minterBundle.publicMintWithBox(1);
     await mint.wait();
 
     expect(await kbox.kBox.balanceOf(ownerAddress)).to.equal(0);
     expect(await kbox.kBox.totalSupply()).to.equal(0);
 
-    // console.log(mint);
     expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(5);
     for (let index = 0; index < 5; index++) {
       expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
     }
-      
+
+    const kbox2 = await ignition.deploy(KBoxModule);
+    const setKBox = await minter.minterBundle.setKBox(kbox2.kBox);
+    const setKboxReceipt = await setKBox.wait();
+
+    expect(await minter.minterBundle.getKBox()).to.equal(kbox2.kBox);
+
+    const mintBox2 = await kbox2.kBox.faucet();
+    await mintBox2.wait();
+
+    expect(await kbox2.kBox.balanceOf(ownerAddress)).to.equal(1);
+    expect(await kbox2.kBox.totalSupply()).to.equal(1);
+    expect(await kbox2.kBox.ownerOf(1)).to.equal(ownerAddress);
+
+    await expect(minter.minterBundle.publicMintWithBox(0)).to.be.reverted;
+
+    const approve2 = await kbox2.kBox.approve(minter.minterBundle, 1);
+    await approve2.wait();
+
+    const mint2 = await minter.minterBundle.publicMintWithBox(1);
+    await mint2.wait();
+
+    expect(await kbox2.kBox.balanceOf(ownerAddress)).to.equal(0);
+    expect(await kbox2.kBox.totalSupply()).to.equal(0);
+
+    expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(10);
+    for (let index = 5; index < 10; index++) {
+      expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
+    }
+  });
+
+  it("Should change the KNFT address", async () => {
+    const { minter, knft, ownerAddress } = await loadFixture(deployModuleFixture);
+
+    const konduxAddress = await minter.minterBundle.getKNFT();
+    const konduxContractAddress = await knft.kondux.target;
+
+    expect(konduxAddress).to.equal(konduxContractAddress);
+
+    const mint = await minter.minterBundle.publicMint({value: ethers.parseEther("0.25")});
+    await mint.wait();
+
+    expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(5);
+    for (let index = 0; index < 5; index++) {
+      expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
+    }
+
+    const knft2 = await ignition.deploy(KNFTModule);
+    const setKNFT = await minter.minterBundle.setKNFT(knft2.kondux);
+    const setKNTReceipt = await setKNFT.wait();
+
+    const grantRole = await knft2.kondux.grantRole(ethers.keccak256(ethers.toUtf8Bytes("MINTER_ROLE")), minter.minterBundle);
+    await grantRole.wait();
+
+    expect(await minter.minterBundle.getKNFT()).to.equal(knft2.kondux);
+
+    const mint2 = await minter.minterBundle.publicMint({value: ethers.parseEther("0.25")});
+    await mint2.wait();
+
+    expect(await knft2.kondux.balanceOf(ownerAddress)).to.equal(5);
+    for (let index = 0; index < 5; index++) {
+      expect(await knft2.kondux.ownerOf(index)).to.equal(ownerAddress);
+    }
+  });
+
+  it("Should change the bundle size", async () => {
+    const { minter, knft, ownerAddress } = await loadFixture(deployModuleFixture);
+
+    const mint = await minter.minterBundle.publicMint({value: ethers.parseEther("0.25")});
+    await mint.wait();
+
+    expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(5);
+    for (let index = 0; index < 5; index++) {
+      expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
+    }
+
+    const setBundleSize = await minter.minterBundle.setBundleSize(10);
+    const setBundleSizeReceipt = await setBundleSize.wait();
+
+    expect(await minter.minterBundle.bundleSize()).to.equal(10);
+
+    const mint2 = await minter.minterBundle.publicMint({value: ethers.parseEther("0.5")});
+    await mint2.wait();
+
+    expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(15);
+    for (let index = 5; index < 15; index++) {
+      expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
+    }
+    
+    // Bundle size cannot be 0
+    await expect(minter.minterBundle.setBundleSize(0)).to.be.reverted;
+
+    // Bundle size cannot be greater than 15
+    await expect(minter.minterBundle.setBundleSize(16)).to.be.reverted;
+
+  });
+
+  it("Should set the DNA of the KNFT after minting", async () => {
+    const { minter, knft } = await loadFixture(deployModuleFixture);
+
+    const mint = await minter.minterBundle.publicMint({value: ethers.parseEther("0.25")});
+    await mint.wait();
+
+    for (let index = 0; index < 5; index++) {
+      expect(await knft.kondux.getDna(index)).to.equal(0);
+    }
+
+    const setDNA = await knft.kondux.setDna(0, ethers.keccak256(ethers.toUtf8Bytes("NEW")));
+    const setDNAReceipt = await setDNA.wait();
+
+    expect(await knft.kondux.getDna(0)).to.equal(ethers.keccak256(ethers.toUtf8Bytes("NEW")));
+
+  });
+
+  it("Should set a Gene at a specific position", async () => {
+    const { minter, knft } = await loadFixture(deployModuleFixture);
+
+    const mint = await minter.minterBundle.publicMint({value: ethers.parseEther("0.25")});
+    await mint.wait();
+
+    for (let index = 0; index < 5; index++) {
+      expect(await knft.kondux.readGene(index, 0, 1)).to.equal(0);
+    }
+
+    const setGene = await knft.kondux.writeGene(0, 1, 0, 1);
+    const setGeneReceipt = await setGene.wait();   
+
+    expect(await knft.kondux.readGene(0, 0, 1)).to.equal(1);
+
+    // Set a gene at a position that does not exist
+    await expect(knft.kondux.writeGene(0, 1, 40, 41)).to.be.reverted;
+
+    // Set a gene at position 20 with 1 byte
+    const setGene2 = await knft.kondux.writeGene(0, 1, 20, 21);
+    await setGene2.wait();
+
+    expect(await knft.kondux.readGene(0, 20, 21)).to.equal(1);
+
+    // Set a Gene at position 10 with 5 bytes (ex.: 0x12593478ff)
+    const setGene3 = await knft.kondux.writeGene(0, 0x12593478ff, 10, 15);
+    await setGene3.wait();
+
+    expect(await knft.kondux.readGene(0, 10, 15)).to.equal(0x12593478ff);
+  });
+
+  it("Should be able to transfer KNFT", async () => {
+    const { minter, knft, ownerAddress } = await loadFixture(deployModuleFixture);
+
+    const mint = await minter.minterBundle.publicMint({value: ethers.parseEther("0.25")});
+    await mint.wait();
+
+    expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(5);
+    for (let index = 0; index < 5; index++) {
+      expect(await knft.kondux.ownerOf(index)).to.equal(ownerAddress);
+    }
+
+    // set a random DNA for the first KNFT
+    const setDNA = await knft.kondux.setDna(0, ethers.keccak256(ethers.toUtf8Bytes("NEW")));
+    await setDNA.wait();
+
+    const [ _, newOwner] = await ethers.getSigners();
+    const newOwnerAddress = await newOwner.getAddress();
+
+    expect(newOwnerAddress).to.not.equal(ownerAddress);
+
+    const transfer = await knft.kondux.transferFrom(ownerAddress, newOwnerAddress, 0);
+    await transfer.wait();
+
+    expect(await knft.kondux.balanceOf(ownerAddress)).to.equal(4);
+    expect(await knft.kondux.balanceOf(newOwnerAddress)).to.equal(1);
+    expect(await knft.kondux.ownerOf(0)).to.equal(newOwnerAddress);
+
+    // DNA should be the same
+    expect(await knft.kondux.getDna(0)).to.equal(ethers.keccak256(ethers.toUtf8Bytes("NEW")));
+
   });
 
 });
