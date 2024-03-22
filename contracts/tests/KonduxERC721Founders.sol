fd8aa89282e3f7fa2b2669cdf22c38c179e7fb54
Marco Jardim <marcoeojardim@gmail.com>
2024-03-22 00:33:25 -0300

    Add burn function to contract for token destruction
    Implemented a new burn function in the contract to allow token owners to destroy their tokens securely. The function verifies the caller's ownership before transferring the token to address 0x2345678901234567890123456789012345678901.

--- contracts/tests/KonduxERC721Founders.sol
+++ contracts/tests/KonduxERC721Founders.sol
@@ -15,4 +15,9 @@ contract KonduxERC721Founders is Ownable, ERC721Enumerable {
         counter++;
         _mint(msg.sender, counter);
     }
+
+    function burn(uint256 tokenId) public {
+        require(ownerOf(tokenId) == msg.sender, "KonduxERC721Founders: caller is not the owner");
+        _transfer(msg.sender, address(0x2345678901234567890123456789012345678901), tokenId);
+    }
 }
